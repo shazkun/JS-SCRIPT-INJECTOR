f7be5a06310d808066652a013375ca6e2f9eb79b
shazkun <seanwiltonr@gmail.com>
2025-01-19 11:08:42 +0800

    test

--- wew.js
+++ wew.js
@@ -1,16 +1,16 @@
-function() {
+(function () {
     var userChoice = prompt('Enter "A" or "B": A = 1ST SEM, B = 2ND SEM:').toUpperCase();
     var userNumber = prompt('Enter the value for SEMESTER YEAR, e.g., "2324":');
-    
+
     var h4Elements = document.getElementsByTagName("h4");
     var firstH4Text = h4Elements[0].textContent || h4Elements[0].innerText;
-    
+
     var lastFourNumbersMatch = firstH4Text.match(/\d{4}(?=\D*$)/);
-    
+
     if (userChoice !== null && userChoice !== '' && (userChoice === 'A' || userChoice === 'B') && userNumber !== null && userNumber !== '') {
         var id = lastFourNumbersMatch ? lastFourNumbersMatch[0] : '';
         var idString = "TUPC-22-" + id;
-        
+
         if (typeof openslip === 'function') {
             openslip(idString, userNumber, userChoice, "Cavite");
         } else {
@@ -19,4 +19,4 @@ function() {
     } else {
         alert('Invalid input');
     }
-}
+})();
